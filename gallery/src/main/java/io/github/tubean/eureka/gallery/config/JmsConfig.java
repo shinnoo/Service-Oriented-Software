4620954d1bfff4d9b62b9b84dcaa7b915f111dd6
trandung <trandung164.ptit@gmail.com>
2021-03-20 21:04:00 +0700

    queue message feature

---
+++ gallery/src/main/java/io/github/tubean/eureka/gallery/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package io.github.tubean.eureka.gallery.config;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+import javax.jms.Queue;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+	@Bean
+	public Queue queue(){
+		return new ActiveMQQueue("test-queue");
+	}
+}
+
